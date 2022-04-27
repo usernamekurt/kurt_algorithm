8d76879fc09929380bdac87930640fe7151a6a36
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-04-27 10:52:14 +0900

    Time: 67 ms (95.99%), Space: 44 MB (93.98%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -1,32 +1,13 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-const isPalindrome = (s) => {
-const regex = /[^A-Za-z0-9]/g;
-const newS = s.toLowerCase().replace(" ", "").replace(regex, "");
-
-let before, after;
-if (newS.length % 2 === 0) {
-  before = (newS.length / 2) - 1;
-  after = before + 1;
-  while (before >= 0) {
-    if (newS[before] !== newS[after]) {
-      return false;
+var isPalindrome = function(s) {
+    let str = s.toLowerCase().replace(/ +/g, "").replace(/[^A-Za-z0-9]/g,"");
+    let start = 0;
+    let end = str.length-1
+    while(start<end){
+      if(str[start] !== str[end]){
+        return false
+      }
+      start ++
+      end --
     }
-    before--;
-    after++;
-  }
-} else {
-  before = Math.floor(newS.length / 2) - 1;
-  after = before + 2;
-  while (before >= 0) {
-    if (newS[before] !== newS[after]) {
-      return false;
-    }
-    before--;
-    after++;
-  }
-}
-return true;
-};
+    return true;
+  };
