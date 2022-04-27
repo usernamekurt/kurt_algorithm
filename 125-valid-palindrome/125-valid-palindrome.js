dec8c433315bad618209441f77ffa4966ab1a4ee
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-04-27 10:52:32 +0900

    Time: 131 ms (18.49%), Space: 44.7 MB (67.17%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -1,13 +1,10 @@
-var isPalindrome = function(s) {
-    let str = s.toLowerCase().replace(/ +/g, "").replace(/[^A-Za-z0-9]/g,"");
-    let start = 0;
-    let end = str.length-1
-    while(start<end){
-      if(str[start] !== str[end]){
-        return false
-      }
-      start ++
-      end --
+function isPalindrome(s) {
+  str = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  if (str.length <= 1) return true;
+  for (let i = 0; i < parseInt(str.length / 2); i++) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
     }
-    return true;
-  };
+  }
+  return true;
+}
