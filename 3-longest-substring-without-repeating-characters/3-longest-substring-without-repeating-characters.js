74dfe0964cffce5bb66aa163073e5c6e48877dc5
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-04 11:27:58 +0900

    Time: 148 ms (46.28%), Space: 47.9 MB (60.35%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -1,29 +1,18 @@
 function lengthOfLongestSubstring(s) {
-  if (s.length === 0) return 0;
-  const dp = new Array(s.length).fill(1);
-  let max = 1;
+  let windowCharsMap = {};
+  let windowStart = 0;
+  let maxLength = 0;
 
-  for (let i = 1; i < s.length; i++) {
-    let currentChr = s[i];
-    let prevIndex = i - 1;
-    let checkContinuous = true;
-    let startIndex;
+  for (let i = 0; i < s.length; i++) {
+    const endChar = s[i];
 
-    for (let j = prevIndex; j > prevIndex - dp[prevIndex]; j--) {
-      if (currentChr === s[j]) {
-        checkContinuous = false;
-        startIndex = j + 1;
-        break;
-      }
+    if (windowCharsMap[endChar] >= windowStart) {
+      windowStart = windowCharsMap[endChar] + 1;
     }
 
-    if (checkContinuous) {
-      dp[i] = dp[prevIndex] + 1;
-      max = Math.max(dp[i], max);
-    } else {
-      dp[i] = i - startIndex + 1;
-    }
+    windowCharsMap[endChar] = i;
+    maxLength = Math.max(maxLength, i - windowStart + 1)
   }
 
-  return max;
+  return maxLength;
 }
