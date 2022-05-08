e1525d70617bdf435c25a29031b4c359347082c4
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-08 13:19:57 +0900

    Time: 92 ms (57.77%), Space: 45.2 MB (33.10%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,11 @@
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) return true;
+  }
+  return false;
+}
