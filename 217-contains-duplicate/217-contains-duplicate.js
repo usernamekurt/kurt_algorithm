fb3ff4718f7b5b45fbb8c95309150f6524559b20
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-08 16:48:53 +0900

    Time: 131 ms (44.39%), Space: 51.6 MB (23.91%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,13 @@
+function containsDuplicate(nums) {
+  const hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (hashMap[num]) {
+      return true;
+    } else {
+      hashMap[num] = true;
+    }
+  }
+  return false;
+}
