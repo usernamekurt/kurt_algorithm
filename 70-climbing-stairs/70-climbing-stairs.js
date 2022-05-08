f9d55afb7bc3fa8d3c346e456b212636be8a6ec6
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-08 09:09:47 +0900

    Time: 68 ms (67.00%), Space: 41.5 MB (90.93%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -1,9 +1,11 @@
-const climbStairs = (n) => {
-  dp = [];
-  dp[0] = 1;
-  dp[1] = 2;
-  for (let i = 2; i < n; i++) {
-    dp[i] = dp[i - 2] + dp[i - 1];
+function climbStairs (n) {
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+
+  let dp = [1, 2]
+
+  for (let i = 2; i < n ; i++) {
+    dp[i] = dp[i - 2] + dp[i - 1]
   }
-  return dp[n-1]
-};
+  return dp.pop()
+}
