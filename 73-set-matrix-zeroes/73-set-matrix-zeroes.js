1793bb5836025d1ee2d6938aaa36d0cce245989b
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-12 16:01:24 +0900

    Time: 126 ms (33.03%), Space: 44.9 MB (57.43%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -1,62 +1,53 @@
 function setZeroes(matrix) {
-  let isFirstRowZero = false;
-  let isFirstColZero = false;
+  let firstColHasZero = false;
+  let firstRowHasZero = false;
 
-  for (let col = 0; col < matrix[0].length; col++) {
-    if (matrix[0][col] === 0) {
-      isFirstRowZero = true;
+  //Check if first column has zero
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
       break;
     }
   }
 
-  for (let row = 0; row < matrix.length; row++) {
-    if (matrix[row][0] === 0) {
-      isFirstColZero = true;
+  //Check if row column has zero
+  for (let i = 0; i < matrix[0].length; i++) {
+    if (matrix[0][i] === 0) {
+      firstRowHasZero = true;
       break;
     }
   }
-  console.log(matrix)
 
+  //Use first row and column as flags,
   for (let row = 1; row < matrix.length; row++) {
     for (let col = 1; col < matrix[0].length; col++) {
       if (matrix[row][col] === 0) {
-        matrix[row][0] = 0;
         matrix[0][col] = 0;
-      }
-    }
-  }
-  console.log(matrix)
-  
-
-  for (let col = 1; col < matrix[0].length; col++) {
-    if (matrix[0][col] === 0) {
-      for (let row = 1; row < matrix.length; row++) {
-        matrix[row][col] = 0;
+        matrix[row][0] = 0;
       }
     }
   }
 
-
+  //Zero out cells based on flags in first row and column
   for (let row = 1; row < matrix.length; row++) {
-    if (matrix[row][0] === 0) {
-      for (let col = 1; col < matrix[0].length; col++) {
+    for (let col = 1; col < matrix[0].length; col++) {
+      if (matrix[row][0] === 0 || matrix[0][col] === 0) {
         matrix[row][col] = 0;
       }
     }
   }
 
-
-  if (isFirstRowZero) {
-    for (let col = 0; col < matrix[0].length; col++) {
-      matrix[0][col] = 0;
+  //Zero out first column
+  if (firstColHasZero) {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i][0] = 0;
     }
   }
 
-  if (isFirstColZero) {
-    for (let row = 0; row < matrix.length; row++) {
-      matrix[row][0] = 0;
+  //Zero out first row
+  if (firstRowHasZero) {
+    for (let i = 0; i < matrix[0].length; i++) {
+      matrix[0][i] = 0;
     }
   }
-
-  return matrix;
 }
