e4332c62b1bd3fb55637b0e20d426ccf67bbe81b
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-17 15:01:23 +0900

    Time: 208 ms (12.93%), Space: 45.5 MB (58.65%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -1,26 +1,12 @@
+var firstUniqChar = function (s) {
+  const m = new Map();
 
-// 1차 Map 자료구조 사용
-var firstUniqChar = function(s) {
-    const charsMap = new Map();
-    let res = -1;
-    
-    //순회하면서 charsMap 채우기
-    // 1차 방문 -> index, 2차 방문 이상 false
-    for (let i = 0; i < s.length; i++) {
-        
-        if (charsMap.get(s[i]) === undefined) {
-            charsMap.set(s[i], i);
-        } else {
-            charsMap.set(s[i], false);
-        };
-    };
-    
-    //hash map 반복문을 통해 값이 false가 아니면 index
-    const valuesOfCharsMap = Array.from(charsMap.values());
-    for (let i = 0; i < valuesOfCharsMap.length; i++) {
-        if (valuesOfCharsMap[i] !== false) {
-            return valuesOfCharsMap[i];
-        }
-    }
-    return -1;
+  for (let i = 0; i < s.length; i++) {
+    m.has(s[i]) ? m.set(s[i], false) : m.set(s[i], i);
+  }
+
+  for (const index of m.values()) {
+    if (index !== false) return index;
+  }
+  return -1;
 };
