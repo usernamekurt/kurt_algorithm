39aca7a678dcb195c0e354fe46fe0863f680b466
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-18 11:45:12 +0900

    Time: 101 ms (39.91%), Space: 46.1 MB (22.55%) - LeetHub

--- 112-path-sum/112-path-sum.js
+++ 112-path-sum/112-path-sum.js
@@ -1,30 +1,22 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} targetSum
- * @return {boolean}
- */
-var hasPathSum = function(root, targetSum) {
-    if (!root) return false
-    let res = false;
-    dfs(root, 0);
-    
-    function dfs (node, sum) {
-        if (res === true) return;
-        sum += node.val;
-        if (node.left === null && node.right === null) {
-            res = (sum === targetSum);
-            return;
-        } 
-        if (node.left) dfs(node.left, sum);
-        if (node.right) dfs(node.right, sum);
+var hasPathSum = function (root, sumTarget) {
+  let res = false;
+
+  function helper(node, currentSum) {
+    if (!node || res) {
+      return;
     }
-    return res;
+
+    currentSum += node.val;
+
+    if (!node.left && !node.right && currentSum === sumTarget) {
+      res = true;
+      return;
+    }
+
+    helper(node.left, currentSum);
+    helper(node.right, currentSum);
+  }
+  
+  helper(root, 0);
+  return res;
 };
