ab48d47e6a852665cfb1bd268b03859ea8b7d4f2
kurt <97776883+usernamekurt@users.noreply.github.com>
2022-05-24 10:20:19 +0900

    Time: 105 ms (87.06%), Space: 43.1 MB (95.08%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -1,25 +1,27 @@
-function longestPalindrome(s) {
-  let maxLen = 0;
-  let maxLeft = 0;
-  let maxRight = 0;
-  
-  for (let i = 0; i < s.length; i++) {
-    searchSameChr(i, i);
-    searchSameChr(i, i+1);
-  }
-
-  function searchSameChr (left, right) {
-    while (left >= 0 && right < s.length && s[left] === s[right]) {
-      const currentLen = right - left + 1;
-      if (currentLen > maxLen) {
-        maxLen = currentLen;
-        maxLeft = left;
-        maxRight = right;
-      }
-      left--;
-      right++;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let maxIndex = 0;
+    let maxLength = 1;
+    
+    function searchFromMiddle (left, right) {
+        let curLength;
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            curLength = right - left + 1;
+            if (curLength > maxLength) {
+                maxIndex = left;
+                maxLength = curLength;
+            }
+            left--;
+            right++;
+        }
     }
-  }
-
-  return s.slice(maxLeft, maxRight+1)
-}
+    
+    for (let i = 1; i < s.length; i++) {
+        searchFromMiddle(i - 1, i );
+        searchFromMiddle(i - 1, i + 1 )
+    }
+    return s.substr(maxIndex, maxLength);
+};
