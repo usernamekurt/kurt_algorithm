8ff66bcea81c739b269dfc9e210437f3e920f8fb
borinawa <97776883+usernamekurt@users.noreply.github.com>
2022-06-10 10:52:35 +0900

    Time: 337 ms (88.93%), Space: 103.8 MB (46.37%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
@@ -1,6 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number[][]} operations
+ * @return {number[]}
+ */
 var arrayChange = function(nums, operations) {
-    operations.forEach(v => {
-        nums[nums.indexOf(v[0])] = v[1];
-    })
+    let idxHashMap = {};
+    for (i = 0; i < nums.length; i++) {
+        idxHashMap[nums[i]] = i;
+    }
+    
+    for (i = 0; i < operations.length; i++) {
+        let index = idxHashMap[operations[i][0]];
+        nums[index] = operations[i][1];
+        idxHashMap[operations[i][1]] = index;
+    }
+    
     return nums;
 };
